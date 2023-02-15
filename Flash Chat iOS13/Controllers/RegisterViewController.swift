4dddbdb0b504c630e1e5453ee9a03cf6ad441ea8
Chima-Anason <tblynxx@gmail.com>
2023-02-15 08:38:06 +0800

    Validation Error message, if password not allowed by Firebase

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -15,12 +15,18 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var validationTextfield: UILabel!
+    
     
     @IBAction func registerPressed(_ sender: UIButton) {
         
         if let email = emailTextfield.text, let password = passwordTextfield.text{
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error{
+                    self.validationTextfield.text = e.localizedDescription
+                    
+                    self.validationTextfield.isHidden = false
+                    
                     print(e.localizedDescription)
                 }else{
                     self.performSegue(withIdentifier: "RegisterToChat", sender: self)
